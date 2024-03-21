c2951276f61809a14f0eeed7ecc214af1206cf5e
Avani Anand <iamavanianand@gmail.com>
2024-03-21 16:23:53 +0530

    "update and added some codes in payments routes"

--- server/controller/payment.controller.js
+++ server/controller/payment.controller.js
@@ -1,6 +1,6 @@
 import User  from "../models/user.model.js";
 import AppError from "../utils/error.utils.js";
-import razorpay from '';
+import razorpay from '../server.js';
 
 
 export const getRazorpayApiKey= async (req,res,next)=>{
@@ -10,6 +10,8 @@ export const getRazorpayApiKey= async (req,res,next)=>{
         key: process.env.RAZORPAY_KEY_ID
     })
 }
+
+
 export const buySubscription= async (req,res,next)=>{
     const {id}=req.user;
     const user = await User.findById(id);
@@ -28,9 +30,33 @@ export const buySubscription= async (req,res,next)=>{
         )
     }
 
+    const subscription=await razorpay.subscriptions.create ({
+        paln_id: process.env.RAZORPAY_PLAN_ID,
+        customer_notify: 1    //after making subscription customer get notify
+
+    });
+//then saving subscription id and status in user inormation
+    user.subscription.id=subscription.id;
+    user.subscription.status=subscription.status;  //here subscription is only inititated so before payment its subscription is in INITIATED or PENDING status after doing payment then its status is became ACTIVE
+
+    await user.save()
+
+    res.status(200).json({
+        success: true,
+        message:'subscribed succesfully',
+        subscription_id:subscription.id
+    })
 
 }
+
+
+
 export const verifySubsciption= async (req,res,next)=>{
+
+    const {id}=req.user;
+    const{razorpay_payment_id,razorpay_signature,razorpay_subscription_id}=req.body;
+
+
     
 }
 export const cancelSubscription= async (req,res,next)=>{
